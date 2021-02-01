964e792f164d34cdbba96b44c4d84be41b872665
Sajjadul <sbappyi200@gmail.com>
2021-02-01 10:26:34 +0600

    multilineString added

---
+++ multilineString.js
@@ -0,0 +1,9 @@
+const firstName = 'sajjadul';
+const lastName = 'isalm';
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+const aboutInfo = `${fullName} is good boy.`;
+console.log(aboutInfo);
+
