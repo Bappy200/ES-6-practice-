9b989628f3955bb4a7beb29cc39bb8ef5f6afb19
Sajjadul <sbappyi200@gmail.com>
2021-02-01 11:36:52 +0600

    destructur added

---
+++ destructur.js
@@ -0,0 +1,12 @@
+// object desturecutr
+const person = {name:'sajjaul', age:21, color:'red'};
+const {name} = person;
+
+console.log(name);
+
+
+// array desturceter
+const number = [1,2,3,4,5];
+const [first, last] = number;
+
+console.log(first, last);
